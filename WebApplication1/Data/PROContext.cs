4b08cf94ef8ded9be5c0fa4fa1471cf993b35cf5
jaykh <jaykh@LAPTOP-48MV2THV>
2023-04-08 15:00:55 +0530

    Add project files.

---
+++ WebApplication1/Data/PROContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+namespace WebApplication1.Data
+{
+    public class PROContext : DbContext
+    {
+        public PROContext(DbContextOptions<PROContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students{get;set;}
+        public DbSet<Department> Department {get; set;}
+    }
+}
---
+++ WebApplication1/Migrations/20230408063543_initial.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Class = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DeptId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_Department_DeptId",
+                        column: x => x.DeptId,
+                        principalTable: "Department",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_DeptId",
+                table: "Students",
+                column: "DeptId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+        }
+    }
+}
